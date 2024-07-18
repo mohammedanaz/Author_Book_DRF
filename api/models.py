f529b60001a5ec278833af55549bf631e87d42bb
mohammedanaz <mohammedanaz@gmail.com>
2024-07-18 17:18:26 +0530

    author and books model created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f'{ self.first_name} - {self.last_name}'
+    
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete= models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return  f'{self.title} - {self.author}'
