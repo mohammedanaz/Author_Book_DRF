9aa6ef68323a5a0adac006930472575de3349967
mohammedanaz <mohammedanaz@gmail.com>
2024-07-19 23:47:30 +0530

    app = application

--- author_book_drf/wsgi.py
+++ author_book_drf/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'author_book_drf.settings')
 
 application = get_wsgi_application()
+app = application
