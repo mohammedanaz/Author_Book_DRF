8bf61adfd2c8e8ddd410729070a09284b5ad8598
mohammedanaz <mohammedanaz@gmail.com>
2024-07-23 16:47:52 +0530

    removed basic auth from settings

--- author_book_drf/settings.py
+++ author_book_drf/settings.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
